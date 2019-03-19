268ff0c1bd3ceb15af3f8f9781d5619ee17f753e
君塚まどか <kimitaro0415@gmail.com>
2019-03-19 18:15:58 +0900

    プロフィールページde自分の投稿が表示されるようにした

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :set_picture, only: [:show, :edit, :update, :destroy], except: :index
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def index
     @pictures = Picture.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @pictures = @user.pictures
   end
 
   private
