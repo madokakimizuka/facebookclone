21d5dd95c4486bf5b44aea50ceed43ebd78ad8f6
君塚まどか <kimitaro0415@gmail.com>
2019-03-26 17:46:46 +0900

    アップデートができない問題を修正

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,7 +1,7 @@
 module PicturesHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm'
-      confirm_blogs_path
+      confirm_pictures_path
     elsif action_name == 'edit'
       picture_path
     end
